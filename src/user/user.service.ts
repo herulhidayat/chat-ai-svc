2d66a3508b6ce333d2fd7cdf69f0b7c4fd268279
Khairul Hidayat <khairul.hidayat@jkt1.ebdesk.com>
2025-11-16 23:08:22 +0700

    [FEATURE] added auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,10 +2,11 @@ import { HttpException, Inject, Injectable } from "@nestjs/common";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { PrismaService } from "src/common/prisma.service";
 import { ValidationService } from "src/common/validation.services";
-import { RegisterUserRequest, UserResponse } from "src/model/user.model";
+import { LoginUserRequest, RegisterUserRequest, UserResponse } from "src/model/user.model";
 import { Logger } from "winston";
 import { UserValidation } from "./user.validation";
 import * as bcrypt from "bcrypt";
+import { randomUUID } from "crypto";
 @Injectable()
 
 export class UserService {
@@ -15,7 +16,7 @@ export class UserService {
         private prismaService: PrismaService
     ){}
     async register(request: RegisterUserRequest) : Promise<UserResponse> {
-        this.logger.info(`register user ${JSON.stringify(request)}`);
+        this.logger.info(`UserService.register ${JSON.stringify(request)}`);
         const registerRequest:RegisterUserRequest = this.validationService.validate(UserValidation.REGISTER, request);
         
         const totalUserWithSameUsername = await this.prismaService.user.count({
@@ -40,6 +41,48 @@ export class UserService {
         return {
             username: user.username,
             name: user.name
+        }
+    }
+
+    async login(request: LoginUserRequest) : Promise<UserResponse> {
+        this.logger.info(`UserService.login ${JSON.stringify(request)}`);
+        const loginUserRequest:LoginUserRequest = this.validationService.validate(UserValidation.LOGIN, request);
+
+        let user = await this.prismaService.user.findUnique({
+            where: {
+                username: loginUserRequest.username
+            }
+        });
+
+        if (!user) {
+            throw new HttpException(
+                `User or password is invalid`,
+                400
+            )
+        }
+
+        const isPasswordValid = await bcrypt.compare(loginUserRequest.password, user.password);
+
+        if (!isPasswordValid) {
+            throw new HttpException(
+                `User or password is invalid`,
+                400
+            )
+        }
+
+        user = await this.prismaService.user.update({
+            where: {
+                username: loginUserRequest.username
+            },
+            data: {
+                token: randomUUID()
+            }
+        });
+
+        return {
+            username: user.username,
+            name: user.name,
+            token: user.token
         }
     }
 }
--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -1,10 +1,14 @@
 import z, { ZodType } from "zod";
-import { RegisterUserRequest } from "src/model/user.model";
 
 export class UserValidation {
-    static readonly REGISTER: ZodType<RegisterUserRequest> = z.object({
+    static readonly REGISTER: ZodType = z.object({
         username: z.string(),
         password: z.string(),
         name: z.string(),
+    });
+
+    static readonly LOGIN: ZodType = z.object({
+        username: z.string(),
+        password: z.string(),
     });
 }
